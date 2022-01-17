a7db4a2feddd09e311bc5ba8c4f789ccd567ac80
jinnatul <morolswediu@gmail.com>
2022-01-17 23:30:37 +0600

    feat: add cli.js

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+
+const RunCommand = (command) => {
+    try {
+        execSync(`${command}`, {stdio: 'inherit'});
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, error);
+        return false;
+    }
+    return true;
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/jinnatul/node-temp ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const CheckedOut = RunCommand(gitCheckoutCommand);
+if (!CheckedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const InstalledDeps = RunCommand(installDepsCommand);
+if (!InstalledDeps) process.exit(-1);
+
+console.log(`Congratulations! You are ready.`)
+console.log(`cd ${repoName} && npm run dev`);
