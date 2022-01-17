e702fc692be8705f9a5b094077e0f616ca4fd7f7
jinnatul <morolswediu@gmail.com>
2022-01-18 01:44:14 +0600

    feat: add some colors

--- bin/cli.js
+++ bin/cli.js
@@ -25,6 +25,6 @@ const InstalledDeps = RunCommand(installDepsCommand);
 if (!InstalledDeps) process.exit(-1);
 
 console.log(`\n${"\033[32m"} Congratulations! You are ready.`)
-console.log('\n%c Morol', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+console.log(`\n${"\033[33m"} This node.js template maintained by Morol`);
 console.log(`\n${"\033[35m"} Dev -> cd ${repoName} && npm run dev`);
-console.log(`\n${"\033[31m"} Prod -> cd ${repoName} && npm start`);
+console.log(`\n${"\033[31m"} Prod -> cd ${repoName} && npm start\n`);
